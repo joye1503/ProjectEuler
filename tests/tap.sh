ceacc6219a0e4c01429374bf1d5e4eae8714981b
jeremylt <jeremy.thompson@colorado.edu>
2020-01-06 21:52:47 -0700

    Testing - update tap.sh for robustness

--- tests/tap.sh
+++ tests/tap.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+ulimit -c 0 # Do not dump core
+
 printf "1..3\n";
 
 # Capture output
@@ -5,7 +9,7 @@ tmpfiles="${output} ${output}.out ${output}.diff ${output}.err SESSION.NAME"
 trap 'rm -f ${tmpfiles}' EXIT
 
 # Run in subshell
-(build/$1 || false) > ${output}.out 2> ${output}.err
+(build/"$1" || false) > "${output}".out 2> "${output}".err
 status=$?
 
 # status
@@ -16,30 +20,30 @@ else
 fi
 
 # stdout
-if [ -f tests/output/$1.out ]; then
-    if diff -u tests/output/$1.out ${output}.out > ${output}.diff; then
+if [ -f tests/output/"$1".out ]; then
+    if diff -u tests/output/"$1".out "${output}".out > "${output}".diff; then
         printf "ok 2 $i1 $1 stdout\n"
     else
         printf "not ok 2 $i1 $1 stdout\n"
         while read line; do
             printf "# ${line}\n"
-        done < ${output}.diff
+        done < "${output}".diff
     fi
-elif [ -s ${output}.out ]; then
+elif [ -s "${output}".out ]; then
     printf "not ok 2 $i1 $1 stdout\n"
     while read line; do
         printf "# + ${line}\n"
-    done < ${output}.out
+    done < "${output}".out
 else
     printf "ok 2 $i1 $1 stdout\n"
 fi
 
 # stderr
-if [ -s ${output}.err ]; then
+if [ -s "${output}".err ]; then
     printf "not ok 3 $i2 $1 stderr\n"
     while read line; do
         printf "# +${line}\n"
-    done < ${output}.err
+    done < "${output}".err
 else
     printf "ok 3 $i2 $1 stderr\n"
 fi
